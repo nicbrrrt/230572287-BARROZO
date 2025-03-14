4e9a1ff9494233feea915a55382defdaacc8cb35
nicbrrrt <nicbarrozo12@gmail.com>
2025-03-14 13:45:17 +0800

    Added files for PHP Exercises

--- PHP/PHPActivity.php
+++ PHP/PHPActivity.php
@@ -22,27 +22,29 @@
         echo "<h2>Exercise 2: Conditional Statements</h2>";
         
         #Exericse 2: N0.1
+
+          $number = 5; 
+
+        
         if ($number % 2 == 0) {
             echo "The number $number is even.<br>";
-        }
-        else {
+        } else {
             echo "The number $number is odd.<br>";
         }
 
-        #Exercise 2: NO.2
+        
         if ($number > 0) {
-            echo $number . "is positive.<br>";
-        }
-        elseif ($number < 0) {
-            echo $number . "is negative.<br>";
-        }
-        else {
-            echo $number . "is zero.<br>";
+            echo $number . " is positive.<br>";
+        } elseif ($number < 0) {
+            echo $number . " is negative.<br>";
+        } else {
+            echo $number . " is zero.<br>";
         }
 
         echo "<h2>Exercise 3: Loops</h2>";
 
-        #Exercise 3: NO.1
+        #Exericse 3: N0.1
+        
         for ($i = 1; $i <= 100; $i++) {
             if ($i % 3 == 0 && $i % 5 == 0) {
                 echo "FizzBuzz\n";
@@ -54,6 +56,59 @@
                 echo $i . "\n";
             }
         }
+        
+        #Exericse 3: N0.2
+
+        function fibonacciEvenNumbers($n) {
+            $fib = [0, 1]; 
+        
+            for ($i = 2; $i < $n; $i++) {
+                $fib[$i] = $fib[$i - 1] + $fib[$i - 2]; 
+            }
+        
+            // Print only the even numbers in the sequence
+            echo "Even Fibonacci numbers: ";
+            foreach ($fib as $num) {
+                if ($num % 2 == 0) {
+                    echo $num . " ";
+                }
+            }
+        }
+        
+        // Call the function for the first 10 Fibonacci numbers
+        fibonacciEvenNumbers(10);
+
+        echo "<h2>Exercise 4: Function</h2>";
+
+        #Exericse 4: N0.1
+
+        function greet($name) {
+            return "Hello, $name! Welcome!";
+        }
+
+        function square($num) {
+            return $num * $num;
+        }
+
+        echo greet("John") . "<br>"; 
+        echo "The square of 4 is " . square(4) . "<br>"; 
+
+        echo "<h2>Exercise 4: Form Handling</h2>";
+
+        #Exericse 5: N0.1
+
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST["name"];
+            echo "<p>Hello, $name!</p>";
+        }
+
     ?>  
+
+        <form method="post">
+                Name: <input type="text" name="name">
+                <input type="submit" value="Submit">
+            </form>
+
 </body>
 </html>
